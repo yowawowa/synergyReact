b0e80bd46ef078492c07241b3163250018301339
Vladislav Kostin <92290777+yowawowa@users.noreply.github.com>
2023-09-18 16:45:58 +0500

    lesson 5: styled-components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
 import { Component } from 'react';
+import Clicker from './components/clicker/clicker';
 
 class App extends Component {
-  state = {
-    counter: 0
+  render() {
+    return (
+      <div className="App">
+        <Clicker />
+      </div>
+    );
   }
-
-  onClickButton = () => {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-
-    render() {
-      let counter = this.state.counter
-      return (
-        <div className="App">
-          <p>You clicked {counter} times</p>
-          <button onClick={() => { this.onClickButton() }}>click here</button>
-        </div>
-      );
-    }
-  }
-
-
+}
 export default App;
---
+++ src/components/clicker/clicker.js
@@ -0,0 +1,54 @@
+import {Component} from 'react';
+import './clicker.css'
+import styled from "styled-components";
+
+class Clicker extends Component {
+    state = {
+        counter: 0
+    }
+
+    onClickButton = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+
+    render() {
+        let counter = this.state.counter
+
+        const Button = styled.button`
+            --accent-color: #FE5F55;
+            background: transparent;
+            border-radius: 3px;
+            border: 2px solid var(--accent-color);
+            color: var(--accent-color);
+            display: inline-block;
+            margin: 0.5rem 1rem;
+            padding: 0.5rem 0;
+            transition: all 200ms ease-in-out;
+            width: 11rem; 
+            background: ${props => props.$primary ? "#FFAFF0" : "#5C5D67"};
+            color: ${props => props.$primary ? "#5C5D67" : "#FFAFF0"};         
+        `
+        const AnotherButton = styled(Button)`
+            border-color: grey;
+            color: #6BA292;    
+        `
+        return (
+            <div className="Clicker">
+                <p>You clicked {counter} times</p>
+                <button onClick={() => {
+                    this.onClickButton()
+                }}>click here
+                </button>
+                <Button>Button</Button>
+                <AnotherButton>Another Button</AnotherButton>
+                <Button $primary>Third Button</Button>
+                <Button as="a" href="#">Link with Button styles</Button>
+            </div>
+        );
+    }
+}
+
+
+export default Clicker;
